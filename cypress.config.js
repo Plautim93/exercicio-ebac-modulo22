53be6fa092ef1242b218db570bfa3137996e9b84
Plautim93 <samuelplautobn@gmail.com>
2026-08-03 12:33:40 -0300

    fix: corrige baseURL do config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
+    pageLoadTimeout: 120000,
+    defaultCommandTimeout: 30000,
     setupNodeEvents(on, config) {
-      
     },
   },
 });
